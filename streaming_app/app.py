2639361fac47dd3b9b046f0349c82e902a6ea39f
HASSEN Sana <sana.hassen@forvia.com>
2023-10-08 15:15:30 +0200

    split topics to 3 and create sending data to hdfs queries

--- streaming_app/app.py
+++ streaming_app/app.py
@@ -7,6 +7,11 @@ from pyspark.sql.functions import *
 
 
 
+output_path_bikes = "hdfs://hdfs-namenode:9000/data/bikes_result"
+output_path_bikeshops = "hdfs://hdfs-namenode:9000/data/bikeshops_result"
+output_path_orders = "hdfs://hdfs-namenode:9000/data/orders_result"
+
+
 spark = SparkSession \
     .builder \
     .getOrCreate()
@@ -18,21 +23,41 @@ df = spark \
   .option("subscribe", "bikes,bikeshops,orders") \
   .load()
 
+
 table = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)" , "topic")
 
 
+# Modify the "path" option in the writeStream operation for each topic
+query_bikes = table.filter(col("topic") == "bikes").writeStream \
+    .outputMode("append") \
+    .format("csv") \
+    .option("checkpointLocation", "checkpoint_bikes") \
+    .option("path", output_path_bikes) \
+    .start()
 
-# Define a streaming query to write to HDFS
-query = table.writeStream \
-  .outputMode("append") \
-  .format("csv") \
-  .option("checkpointLocation", "checkpoint") \
-  .option("path", "hdfs://hdfs-namenode:9000/data/result") \
-  .start()
+query_bikeshops = table.filter(col("topic") == "bikeshops").writeStream \
+    .outputMode("append") \
+    .format("csv") \
+    .option("checkpointLocation", "checkpoint_bikeshops") \
+    .option("path", output_path_bikeshops) \
+    .start()
 
+query_orders = table.filter(col("topic") == "orders").writeStream \
+    .outputMode("append") \
+    .format("csv") \
+    .option("checkpointLocation", "checkpoint_orders") \
+    .option("path", output_path_orders) \
+    .start()
+
+# Await termination for each query
 
 
 # query = table.writeStream.outputMode("append").format("console").start()
 
-# query = table.writeStream.outputMode("append").format("console").start()
-query.awaitTermination()
+f = open("healthy", "w")
+f.write("healthy")
+f.close()
+
+query_bikes.awaitTermination()
+query_bikeshops.awaitTermination()
+query_orders.awaitTermination()
